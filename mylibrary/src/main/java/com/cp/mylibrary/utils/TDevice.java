5f5b0d8c3edddf2ad6a91fc72442acddc7520111
cp <467471641@qq.com>
2018-10-30 10:26:36 +0800

    cc

--- mylibrary/src/main/java/com/cp/mylibrary/utils/TDevice.java
+++ mylibrary/src/main/java/com/cp/mylibrary/utils/TDevice.java
@@ -39,6 +39,7 @@ import java.io.File;
 import java.lang.reflect.Field;
 import java.text.NumberFormat;
 import java.util.List;
+import java.util.UUID;
 
 
 @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
@@ -239,6 +240,30 @@ public class TDevice {
 
 
 	}
+
+
+
+
+
+	 public static String getUUID(Context context)
+	 {
+
+		 final TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+		 final String tmDevice, tmSerial, tmPhone, androidId;
+		 tmDevice = "" + tm.getDeviceId();
+		 tmSerial = "" + tm.getSimSerialNumber();
+		 androidId = "" + android.provider.Settings.Secure.getString(context.getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+
+		 UUID deviceUuid = new UUID(androidId.hashCode(), ((long)tmDevice.hashCode() << 32) | tmSerial.hashCode());
+		 String uniqueId = deviceUuid.toString();
+
+		 return uniqueId;
+
+	 }
+
+
+
 
 
 
