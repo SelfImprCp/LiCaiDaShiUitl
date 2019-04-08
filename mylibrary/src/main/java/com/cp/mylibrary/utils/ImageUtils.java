57dbff0441f095b459d2e176ed0f49b7683c7049
cp <467471641@qq.com>
2019-04-08 10:15:55 +0800

    发包jj

--- mylibrary/src/main/java/com/cp/mylibrary/utils/ImageUtils.java
+++ mylibrary/src/main/java/com/cp/mylibrary/utils/ImageUtils.java
@@ -289,23 +289,36 @@ public class ImageUtils {
      */
     @SuppressWarnings("deprecation")
     public static String getAbsoluteImagePath(Activity context, Uri uri) {
-        String imagePath = "";
-        String[] proj = {MediaStore.Images.Media.DATA};
-        Cursor cursor = context.managedQuery(uri, proj, // Which columns to
-                // return
-                null, // WHERE clause; which rows to return (all rows)
-                null, // WHERE clause selection arguments (none)
-                null); // Order-by clause (ascending by name)
+//        String imagePath = "";
+//        String[] proj = {MediaStore.Images.Media.DATA};
+//        Cursor cursor = context.managedQuery(uri, proj, // Which columns to
+//                // return
+//                null, // WHERE clause; which rows to return (all rows)
+//                null, // WHERE clause selection arguments (none)
+//                null); // Order-by clause (ascending by name)
+//
+//        if (cursor != null) {
+//            int column_index = cursor
+//                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+//            if (cursor.getCount() > 0 && cursor.moveToFirst()) {
+//                imagePath = cursor.getString(column_index);
+//            }
+//        }
+//
+//        return imagePath;
 
-        if (cursor != null) {
-            int column_index = cursor
-                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            if (cursor.getCount() > 0 && cursor.moveToFirst()) {
+        String imagePath = "";
+        String[] proj = new String[]{"_data"};
+        Cursor cursor = context.managedQuery(uri, proj, (String)null, (String[])null, (String)null);
+        if(cursor != null) {
+            int column_index = cursor.getColumnIndexOrThrow("_data");
+            if(cursor.getCount() > 0 && cursor.moveToFirst()) {
                 imagePath = cursor.getString(column_index);
             }
         }
 
         return imagePath;
+
     }
 
 
