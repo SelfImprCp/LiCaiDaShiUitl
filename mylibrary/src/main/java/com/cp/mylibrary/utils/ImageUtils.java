cc2fd90a4510e641add32f19fc2ff30087e8343c
cp <467471641@qq.com>
2019-04-08 15:40:44 +0800

    发包jj

--- mylibrary/src/main/java/com/cp/mylibrary/utils/ImageUtils.java
+++ mylibrary/src/main/java/com/cp/mylibrary/utils/ImageUtils.java
@@ -348,24 +348,24 @@ public class ImageUtils {
     public static String getFilePathByUri(Context context, Uri uri) {
         String path = null;
         // 以 file:// 开头的
-      //  if (ContentResolver.SCHEME_FILE.equals(uri.getScheme())) {
+         if (ContentResolver.SCHEME_FILE.equals(uri.getScheme())) {
             path = uri.getPath();
             return path;
-        //}
+       }
         // 以 content:// 开头的，比如 content://media/extenral/images/media/17766
-//        if (ContentResolver.SCHEME_CONTENT.equals(uri.getScheme()) && Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
-//            Cursor cursor = context.getContentResolver().query(uri, new String[]{MediaStore.Images.Media.DATA}, null, null, null);
-//            if (cursor != null) {
-//                if (cursor.moveToFirst()) {
-//                    int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-//                    if (columnIndex > -1) {
-//                        path = cursor.getString(columnIndex);
-//                    }
-//                }
-//                cursor.close();
-//            }
-//            return path;
-//        }
+        if (ContentResolver.SCHEME_CONTENT.equals(uri.getScheme()) && Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            Cursor cursor = context.getContentResolver().query(uri, new String[]{MediaStore.Images.Media.DATA}, null, null, null);
+            if (cursor != null) {
+                if (cursor.moveToFirst()) {
+                    int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                    if (columnIndex > -1) {
+                        path = cursor.getString(columnIndex);
+                    }
+                }
+                cursor.close();
+            }
+            return path;
+        }
 //        // 4.4及之后的 是以 content:// 开头的，比如 content://com.android.providers.media.documents/document/image%3A235700
 //        if (ContentResolver.SCHEME_CONTENT.equals(uri.getScheme()) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
 //         //   if (DocumentsContract.isDocumentUri(context, uri)) {
@@ -387,7 +387,7 @@ public class ImageUtils {
 //
 //       //     }
 //        }
-//        return null;
+        return null;
     }
 
     private static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
