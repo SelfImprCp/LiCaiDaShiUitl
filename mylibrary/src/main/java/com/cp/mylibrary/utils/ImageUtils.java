f47079412c3b70f1156528fc180ea50370dfdbe6
cp <467471641@qq.com>
2019-04-08 12:29:47 +0800

    发包jj

--- mylibrary/src/main/java/com/cp/mylibrary/utils/ImageUtils.java
+++ mylibrary/src/main/java/com/cp/mylibrary/utils/ImageUtils.java
@@ -348,46 +348,46 @@ public class ImageUtils {
     public static String getFilePathByUri(Context context, Uri uri) {
         String path = null;
         // 以 file:// 开头的
-        if (ContentResolver.SCHEME_FILE.equals(uri.getScheme())) {
+      //  if (ContentResolver.SCHEME_FILE.equals(uri.getScheme())) {
             path = uri.getPath();
             return path;
-        }
+        //}
         // 以 content:// 开头的，比如 content://media/extenral/images/media/17766
-        if (ContentResolver.SCHEME_CONTENT.equals(uri.getScheme()) && Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
-            Cursor cursor = context.getContentResolver().query(uri, new String[]{MediaStore.Images.Media.DATA}, null, null, null);
-            if (cursor != null) {
-                if (cursor.moveToFirst()) {
-                    int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-                    if (columnIndex > -1) {
-                        path = cursor.getString(columnIndex);
-                    }
-                }
-                cursor.close();
-            }
-            return path;
-        }
-        // 4.4及之后的 是以 content:// 开头的，比如 content://com.android.providers.media.documents/document/image%3A235700
-        if (ContentResolver.SCHEME_CONTENT.equals(uri.getScheme()) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-         //   if (DocumentsContract.isDocumentUri(context, uri)) {
-
-                    // MediaProvider
-                    final String docId = DocumentsContract.getDocumentId(uri);
-                    final String[] split = docId.split(":");
-                    final String type = split[0];
-                LogCp.i(LogCp.CP,ImageUtils.class + " image Type : " + type);
-
-                Uri contentUri = null;
-                    if ("image".equals(type)) {
-                        contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                    }
-                    final String selection = "_id=?";
-                    final String[] selectionArgs = new String[]{split[1]};
-                    path = getDataColumn(context, contentUri, selection, selectionArgs);
-                    return path;
-
-       //     }
-        }
-        return null;
+//        if (ContentResolver.SCHEME_CONTENT.equals(uri.getScheme()) && Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+//            Cursor cursor = context.getContentResolver().query(uri, new String[]{MediaStore.Images.Media.DATA}, null, null, null);
+//            if (cursor != null) {
+//                if (cursor.moveToFirst()) {
+//                    int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+//                    if (columnIndex > -1) {
+//                        path = cursor.getString(columnIndex);
+//                    }
+//                }
+//                cursor.close();
+//            }
+//            return path;
+//        }
+//        // 4.4及之后的 是以 content:// 开头的，比如 content://com.android.providers.media.documents/document/image%3A235700
+//        if (ContentResolver.SCHEME_CONTENT.equals(uri.getScheme()) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//         //   if (DocumentsContract.isDocumentUri(context, uri)) {
+//
+//                    // MediaProvider
+//                    final String docId = DocumentsContract.getDocumentId(uri);
+//                    final String[] split = docId.split(":");
+//                    final String type = split[0];
+//                LogCp.i(LogCp.CP,ImageUtils.class + " image Type : " + type);
+//
+//                Uri contentUri = null;
+//                    if ("image".equals(type)) {
+//                        contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+//                    }
+//                    final String selection = "_id=?";
+//                    final String[] selectionArgs = new String[]{split[1]};
+//                    path = getDataColumn(context, contentUri, selection, selectionArgs);
+//                    return path;
+//
+//       //     }
+//        }
+//        return null;
     }
 
     private static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
